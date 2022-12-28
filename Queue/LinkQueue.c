d1b6636fdfbe268e124e87c3b0e2625ad3ab2f50
Zkb <1453335882@qq.com>
2022-12-28 16:38:40 +0800

    队列

---
+++ Queue/LinkQueue.c
@@ -0,0 +1,71 @@
+/*
+ * @Descripttion:链式队列
+ * @Author: zkb
+ * @Date: 2022-12-28 15:22:14
+ * @LastEditors: zkb
+ * @LastEditTime: 2022-12-28 16:18:20
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElemType;
+typedef struct LinkNode
+{
+    ElemType data;
+    struct LinkNode *next;
+} LinkNode;
+typedef struct
+{
+    LinkNode *rear, *front;
+} LinkQueue;
+
+void InitQueue(LinkQueue *Q)
+{
+    Q->front = Q->rear = (LinkNode *)malloc(sizeof(LinkNode));
+    Q->front->next = NULL;
+}
+
+int IsEmpty(LinkQueue Q)
+{
+    if (Q.front == Q.rear)
+        return 1;
+    else
+        return 0;
+}
+
+void EnQueue(LinkQueue *Q, ElemType x)
+{
+    LinkNode *p = (LinkNode *)malloc(sizeof(LinkNode));
+    p->data = x;
+    p->next = NULL;
+    Q->rear->next = p;
+    Q->rear = p;
+}
+
+void DeQueue(LinkQueue *Q, ElemType *x)
+{
+    if (Q->front == Q->rear)
+    {
+        printf("队空！\n");
+        return;
+    }
+    LinkNode *p = Q->front->next;
+    *x = p->data;
+    Q->front->next = p->next;
+    if (Q->rear == p)
+    {
+        Q->rear = Q->front;
+    }
+    free(p);
+}
+int main()
+{
+    LinkQueue Q;
+    InitQueue(&Q);
+    EnQueue(&Q, 1);
+    EnQueue(&Q, 2);
+    EnQueue(&Q, 3);
+    int tmp;
+    DeQueue(&Q, &tmp);
+    return 0;
+}
---
+++ Queue/SqQueue.c
@@ -0,0 +1,72 @@
+/*
+ * @Descripttion:循环队列
+ * @Author: zkb
+ * @Date: 2022-12-28 14:19:25
+ * @LastEditors: zkb
+ * @LastEditTime: 2022-12-28 15:10:31
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MaxSize 5
+typedef int ElemType;
+typedef struct
+{
+    ElemType data[MaxSize];
+    int front, rear;
+} SqQueue;
+
+void InitQueue(SqQueue *Q)
+{
+
+    //*Q->data = {0};
+    memset(Q->data, 0, MaxSize);
+    Q->front = Q->rear = 0; // 队空 front==rear
+}
+
+/**
+ * @msg: 入队
+ * @param {SqQueue} *Q
+ * @param {ElemType} x
+ * @return {*}
+ */
+int EnQueue(SqQueue *Q, ElemType x)
+{
+    if ((Q->rear + 1) % MaxSize == Q->front)
+    {
+        printf("队满！\n");
+        return 0;
+    }
+    Q->data[Q->rear] = x;
+    Q->rear = (Q->rear + 1) % MaxSize;
+    return 1;
+}
+
+int DeQueue(SqQueue *Q, ElemType *x)
+{
+    if (Q->front == Q->rear)
+    {
+        printf("队空!\n");
+        return 0;
+    }
+    *x = Q->data[Q->front];
+    Q->data[Q->front] = 0;
+    Q->front = (Q->front + 1) % MaxSize;
+    return 1;
+}
+int main()
+{
+    SqQueue Q;
+    InitQueue(&Q);
+    EnQueue(&Q, 1);
+    EnQueue(&Q, 2);
+    EnQueue(&Q, 3);
+    EnQueue(&Q, 4);
+    EnQueue(&Q, 5);
+    int out;
+    DeQueue(&Q, &out);
+    DeQueue(&Q, &out);
+
+    return 0;
+}
